19174338bd7e0f5134211b34a9f9e4bfed35d13d
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-08-13 14:04:58 +0100

    Adding a product function added

--- src/main/java/com/enset/architecturejee_tp2/web/ProductController.java
+++ src/main/java/com/enset/architecturejee_tp2/web/ProductController.java
@@ -3,9 +3,12 @@ package com.enset.architecturejee_tp2.web;
 
 import com.enset.architecturejee_tp2.entities.Product;
 import com.enset.architecturejee_tp2.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -24,9 +27,31 @@ public class ProductController {
 
         return "products";
     }
+
+    @GetMapping("/")
+    public String index() {
+
+        return "redirect:/index";
+    }
+
+
     @GetMapping("/delete")
     public String delete(@RequestParam(name="id") Long id) {
         productRepository.deleteById( id);
+        return "redirect:/index";
+    }
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+
+    }
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult,Model model) {
+        if (bindingResult.hasErrors()) {
+            return "new-product";
+        }
+        productRepository.save(product);
         return "redirect:/index";
     }
 }
