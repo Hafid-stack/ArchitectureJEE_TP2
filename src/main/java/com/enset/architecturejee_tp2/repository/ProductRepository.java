cc89a9549a6071be8fbbe4d81a994ebffecf022b
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-08-13 12:48:45 +0100

    Adding delete function

--- src/main/java/com/enset/architecturejee_tp2/repository/ProductRepository.java
+++ src/main/java/com/enset/architecturejee_tp2/repository/ProductRepository.java
@@ -3,8 +3,10 @@ package com.enset.architecturejee_tp2.repository;
 import com.enset.architecturejee_tp2.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
-
+    List<Product> id(long id);
 }
--- src/main/java/com/enset/architecturejee_tp2/web/ProductController.java
+++ src/main/java/com/enset/architecturejee_tp2/web/ProductController.java
@@ -6,6 +6,7 @@ import com.enset.architecturejee_tp2.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,5 +23,10 @@ public class ProductController {
         model.addAttribute("products", products);
 
         return "products";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name="id") Long id) {
+        productRepository.deleteById( id);
+        return "redirect:/index";
     }
 }
